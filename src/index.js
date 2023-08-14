5a507b10225ed61eb7af24a2a4fd8681ac94188d
ritik yadav <yadavritik467@gmail.com>
2023-08-14 17:58:33 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';  
 import './index.css';
 import App from './App';
 import Context from './context/contex';
 import { AuthProvider } from './context/auth';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <GoogleOAuthProvider clientId="659252251104-ahf20a559sph3a6864c7blc3lgsj0061.apps.googleusercontent.com">
   <React.StrictMode>
     <Context>
     <AuthProvider>
-        <App />
+       
+            <App />
+       
       </AuthProvider>
     </Context>
   </React.StrictMode>
+    </GoogleOAuthProvider>
 );
 
 
